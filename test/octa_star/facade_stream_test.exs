cf13bfede58ed764025270c85d1cd12f10666b5f
Mykolas Mankevicius <hello@themykolas.com>
2026-05-15 20:54:13 +0300

    Add more tests

---
+++ test/octa_star/facade_stream_test.exs
@@ -0,0 +1,30 @@
+defmodule OctaStar.FacadeStreamTest do
+  use ExUnit.Case, async: false
+
+  import Plug.Test
+
+  alias OctaStar.TestAssertions
+  alias OctaStar.Utility.StreamRegistry
+
+  setup do
+    {:ok, _} = start_supervised({Registry, keys: :unique, name: StreamRegistry})
+    :ok
+  end
+
+  test "start_stream/2 delegates to StreamRegistry and opens SSE" do
+    tab_id = "facade-tab"
+    query = URI.encode_query(%{"datastar" => ~s({"tabId":"#{tab_id}"})})
+
+    conn =
+      :get
+      |> conn("/?#{query}")
+      |> OctaStar.start_stream(:user_1)
+
+    assert conn.state == :chunked
+    assert [{pid, nil}] = Registry.lookup(StreamRegistry, {:user_1, tab_id})
+    assert pid == self()
+
+    assert {200, headers, _body} = TestAssertions.chunked_resp(conn)
+    assert {"content-type", "text/event-stream; charset=utf-8"} in headers
+  end
+end
